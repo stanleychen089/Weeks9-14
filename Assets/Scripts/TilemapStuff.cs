085490bce016d75987c51bc59a09d8bb40c790f9
chensta <chensta@OA-S138-04>
2026-03-29 22:24:54 -0400

    Add a tilemap, where clicking on a grass tile turns it into a flower tile, and a transparent square follows the cell the mouse is under to highlight the selected tile

---
+++ Assets/Scripts/TilemapStuff.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class TilemapStuff : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Transform highlight;
+
+    public Tile flower; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3Int cellPos = tilemap.WorldToCell(mousePos);
+        Vector3 pos = tilemap.GetCellCenterWorld(cellPos);
+        //Debug.Log(mousePos + " is at cell: " + cellPos);
+        highlight.position = pos; 
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log(tilemap.GetTile(cellPos)); 
+            tilemap.SetTile(cellPos, flower);
+        }
+    }
+}
