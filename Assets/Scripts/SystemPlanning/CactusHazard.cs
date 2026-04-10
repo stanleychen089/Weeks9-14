931bb7007efc487706c964816da23a6ac15dca67
chensta <chensta@OA-S138-04>
2026-04-10 19:27:20 -0400

    Added cactuses to the scene. If the player touches the cactus, their speed is lowered until they get out, in which a timer will start to indicate that they have exited.

---
+++ Assets/Scripts/SystemPlanning/CactusHazard.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusHazard : MonoBehaviour
+{
+    public bool isInHazard = false;
+    public List<SpriteRenderer> cactusSRs;
+    public DuckisaurMovement playerMovement;
+    public float penaltyTime = 2;
+    public float defaultSpeed = 5;
+    public float t = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isInHazard = false;
+        //for each line used from Deadly Dungeon Challenge from week 7
+        foreach (SpriteRenderer sr in cactusSRs)
+        {
+            if (sr.bounds.Contains(transform.position))
+            {
+                isInHazard = true;
+            }
+        }
+        if (isInHazard)
+        {
+            HazardEffect();
+            t = 0;
+        }
+        else
+        {
+            t += Time.deltaTime;
+            if (t > penaltyTime)
+            {
+                playerMovement.speed = defaultSpeed;
+            }
+        }
+    }
+
+    void HazardEffect()
+    {
+        playerMovement.speed = 2;
+
+    }
+}
